0f0b3fc28c27af8476f5bf76885fa1d8254c9f0a
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2024-08-11 12:05:01 +0300

    Moved func

--- script/src/app/getFiles.js
+++ script/src/app/getFiles.js
@@ -1,6 +1,6 @@
 import fsAsync from "node:fs/promises";
 import path from "path";
-import { getHashReq } from "../api/getHashReq.js";
+import { getHash } from "./getHash.js";
 
 export const getFiles = async (dirPath, ignore) => {
   const stack = [dirPath];
@@ -45,29 +45,7 @@ export const getFiles = async (dirPath, ignore) => {
     }
   }
 
-  console.info("## get hash");
-  const pathToFiles = driveRes.map(item => item.path);
-  const promises = [];
-
-  const quantity = 1000
-
-  for (let i = 0; i < Math.ceil(pathToFiles.length / quantity); i++) {
-    const slice = pathToFiles.slice(i * quantity, (i + 1) * quantity);
-
-    promises.push(getHashReq(slice));
-  }
-
-  const results = await Promise.all(promises);
-
-  let resultIndex = 0;
-  for (let i = 0; i < Math.ceil(pathToFiles.length / quantity); i++) {
-    const slice = pathToFiles.slice(i * quantity, (i + 1) * quantity);
-
-    for (let j = 0; j < slice.length; j++) {
-      driveRes[resultIndex].hash = results[i][j];
-      resultIndex++;
-    }
-  }
+  await getHash(driveRes)
 
   return driveRes;
 };
